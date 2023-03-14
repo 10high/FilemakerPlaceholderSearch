a6318960e7c2cf0adf5761287160ff9579365fa8
Michael Waaler <michael@flyingtens.com>
2023-03-14 15:34:48 +0100

    adds basic ResultsPreview component

---
+++ src/Components/ResultsPreview/ResultsPreview.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Styles from "./Styles.module.css";
+
+export class ResultsPreview extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { previewText: "Your preview will be displayed here" }
+    }
+    handleChange() {
+        //This will handle searching and tagging preview text for display
+    }
+    render() {
+        return (
+            <div className={Styles.preview__Wrapper}>
+                <p>{this.state.previewText}</p>
+            </div>
+        )
+    }
+}
