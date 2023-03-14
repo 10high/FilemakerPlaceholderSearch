6fa69979fe6b44c9f3b7f891203375abe58ac44c
Michael Waaler <michael@flyingtens.com>
2023-03-14 16:03:29 +0100

    adds basic PlaceholderDefinition component

---
+++ src/Components/PlaceholderDefinition/PlaceholderDefinition.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Styles from "./Styles.module.css";
+
+export class PlaceholderDefinition extends React.Component {
+    render() {
+        return (
+            <section className={Styles.section}>
+                <span className={Styles.section__headerWrapper}>
+                    <h2 className={Styles.section__header}>Placeholder Definition</h2>
+                </span>
+                <div className={Styles.section__bodyWrapper}>
+                    <p className={Styles.section__bodyText}>{this.props.placeholderDefinition}</p>
+                </div>
+            </section>
+        )
+    }
+}
