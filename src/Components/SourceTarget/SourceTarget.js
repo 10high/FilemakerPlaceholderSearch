23dbb565f4fa61a3fd9fc74d4e44f3b32f3dbfd1
Michael Waaler <michael@flyingtens.com>
2023-03-14 12:43:18 +0100

    adds basic SourceTarget component

---
+++ src/Components/SourceTarget/SourceTarget.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Styles from "./Styles.module.css";
+
+export class SourceTarget extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { selectedOption: "source" };
+        this.handleOptionChange = this.handleOptionChange.bind(this);
+    }
+
+    handleOptionChange(event) {
+        this.setState({ selectedOption: event.target.value });
+    }
+
+    render() {
+        return (
+            <form className={Styles.form} action="" name="sourceTarget">
+                <fieldset className={Styles.form__wrapper}>
+                    <label className={Styles.form__item}>
+                        <input
+                            type="radio"
+                            name="sourceTarget"
+                            value="source"
+                            checked={this.state.selectedOption === "source"}
+                            onChange={this.handleOptionChange}
+                        />
+                        Source
+                    </label>
+
+                    <label className={Styles.form__item}>
+                        <input
+                            type="radio"
+                            name="sourceTarget"
+                            value="target"
+                            checked={this.state.selectedOption === "target"}
+                            onChange={this.handleOptionChange}
+                        />
+                        Target
+                    </label>
+                </fieldset>
+            </form>
+        )
+    }
+}
+
