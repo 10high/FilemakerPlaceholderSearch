d8987ef693edcc218b6f313803e0b8fd46e863d6
Michael Waaler <michael@flyingtens.com>
2023-03-21 15:32:37 +0100

    adds basic app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,1 +1,38 @@
+import React from "react";
+import Header from "../Header/Header";
+import SearchInput from "../SearchInput/SearchInput";
+import SourceTarget from "../SourceTarget/SourceTarget";
+import PlaceholderSearch from "../PlaceholderSearch/PlaceholderSearch";
+import ResultsList from "../ResultsList/ResultsList";
+import ResultsPreview from "../ResultsPreview/ResultsPreview";
+import PlaceholderDefinition from "../PlaceholderDefinition/PlaceholderDefinition";
 import "../customproperties.css";
+
+export class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <main>
+                    <div>
+                        <SearchInput />
+                        <SourceTarget />
+                    </div>
+                    <div>
+                        <div>
+                            <PlaceholderSearch />
+                            <PlaceholderSearch />
+                            <PlaceholderSearch />
+                            <PlaceholderSearch />
+                            <PlaceholderSearch />
+                            <PlaceholderSearch />
+                        </div>
+                        <ResultsList />
+                    </div>
+                    <ResultsPreview />
+                    <PlaceholderDefinition />
+                </main>
+            </div>
+        );
+    }
+}
