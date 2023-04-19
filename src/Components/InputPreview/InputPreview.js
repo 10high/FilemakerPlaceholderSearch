c6019aeba25e826181dbbb4b1f66550b31aed7b8
Michael Waaler <michael@flyingtens.com>
2023-04-19 09:57:51 +0200

    adds basic InputPreview component

---
+++ src/Components/InputPreview/InputPreview.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Styles from "./InputPreview.module.css"
+
+export default class InputPreview extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
